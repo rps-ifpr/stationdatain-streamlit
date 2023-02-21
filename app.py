43c9d0fe2af99f80400ab54ec6531a3ef0ae975e
user.email <rogerio.dosantos@ifpr.edu.br>
2023-02-21 17:37:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,27 @@ df['Day'] = df['Date/Time'].dt.floor('D')
 # Agrupar os dados por dia e calcular a média da temperatura interna
 daily_avg_temp = df.groupby('Day')['Indoor Temperature(°C)'].mean()
 
-st.write(daily_avg_temp)
+# Criar um menu suspenso para selecionar o período
+periodo = st.sidebar.selectbox('Período', ['Últimos 7 dias', 'Últimos 30 dias', 'Todos os dados'])
+
+# Selecionar o subconjunto apropriado de dados
+if periodo == 'Últimos 7 dias':
+    data = daily_avg_temp.tail(7)
+elif periodo == 'Últimos 30 dias':
+    data = daily_avg_temp.tail(30)
+else:
+    data = daily_avg_temp
+
+# Criar um gráfico de linha com a temperatura média por dia
+fig, ax = plt.subplots()
+ax.plot(daily_avg_temp.index, daily_avg_temp.values)
+ax.set_xlabel('Data')
+ax.set_ylabel('Temperatura Média (°C)')
+ax.set_title('Temperatura Média por Dia')
+ax.grid(True)
+
+# Exibir o gráfico no Streamlit
+st.pyplot(fig)
 
 
 
