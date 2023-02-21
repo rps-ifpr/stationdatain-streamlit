b65b1d2d378086f54a7e23157e4e975447155fbe
user.email <rogerio.dosantos@ifpr.edu.br>
2023-02-21 17:50:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ df= pd.read_csv ('dados.csv', delimiter=';')
 st.title("Dados da estação meteorológica")
 st.write("Aqui estão os dados de estação Local:")
 
-first_10_rows = df.head(10)
-st.write(first_10_rows)
+#first_10_rows = df.head(10)
+#st.write(first_10_rows)
 
 # Converter a coluna de data/hora para o tipo datetime
-df['Date/Time'] = pd.to_datetime(df['Time'])
+df['Date'] = pd.to_datetime(df['Time'])
 
 # Criar uma nova coluna com a data arredondada para o dia mais próximo
-df['Day'] = df['Date/Time'].dt.floor('D')
+df['Day'] = df['Date'].dt.floor('D')
 
 # Agrupar os dados por dia e calcular a média da temperatura interna
 daily_avg_temp = df.groupby('Day')['Indoor Temperature(°C)'].mean()
