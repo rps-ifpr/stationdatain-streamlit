808372b01c0cc06b306b250a9d5c1165bc7fbb48
user.email <rogerio.dosantos@ifpr.edu.br>
2023-02-21 15:45:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 # importando os dados
-data = pd.read_csv ('dados.csv')
+df= pd.read_csv ('dados.csv', delimiter=';')
 
-#gráfico de linhas dos dados de temperatura usando Matplotlib
 st.title("Dados da estação meteorológica")
-st.write("Aqui estão os dados de temperatura:")
-fig, ax = plt.subplots()
-ax.plot(data["Time"], data["Indoor Temperature(°C)"])
-st.pyplot(fig)
+st.write("Aqui estão os dados de estação Local:")
+
+first_10_rows = df.head(10)
+st.write(first_10_rows)
+
+
+
 
 
