4ea68927334ebea4e3d5ad8de224ad1d968700da
user.email <rogerio.dosantos@ifpr.edu.br>
2023-02-21 17:25:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ st.write("Aqui estão os dados de estação Local:")
 first_10_rows = df.head(10)
 st.write(first_10_rows)
 
+# Converter a coluna de data/hora para o tipo datetime
+df['Date/Time'] = pd.to_datetime(df['Time'])
+
+# Criar uma nova coluna com a data arredondada para o dia mais próximo
+df['Day'] = df['Date/Time'].dt.floor('D')
+
+# Agrupar os dados por dia e calcular a média da temperatura interna
+daily_avg_temp = df.groupby('Day')['Indoor Temperature(°C)'].mean()
+
+st.write(daily_avg_temp)
 
 
 
