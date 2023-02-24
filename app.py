806d2c1ff096f34f8e7e85fdd82a181be26d87ea
user.email <rogerio.dosantos@ifpr.edu.br>
2023-02-24 13:21:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,25 +38,40 @@ df_novo = {'n': 'id',
              'Week Rainfall(mm)': 'chuva_semana',
              'Month Rainfall(mm)': 'chuva_mes',
              'Total Rainfall(mm)': 'chuva_total'}
-
 df.info()
 
 # renomeando as colunas
 df.rename(columns=df_novo, inplace=True)
 
+# Definindo a função de remoção de outliers
+def remove_outliers_iqr(df, multiplier=1.5):
+    # Cria uma cópia do dataframe original
+    df_clean = df.copy ()
+    # Itera sobre todas as colunas numéricas
+    for column in df_clean.select_dtypes (include='number').columns:
+        # Calcula os limites inferior e superior usando o IQR
+        Q1 = df_clean[column].quantile (0.25)
+        Q3 = df_clean[column].quantile (0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - multiplier * IQR
+        upper_bound = Q3 + multiplier * IQR
+        # Remove os outliers
+        df_clean = df_clean[(df_clean[column] >= lower_bound) & (df_clean[column] <= upper_bound)]
+    return df_clean
+# Removendo os outliers
+df_clean = remove_outliers_iqr(df)
+
 # selecionando apenas colunas numéricas
 num_cols = df.select_dtypes(include=['float64', 'int64']).columns
 
 # removendo colunas 'id' e 'Intervalo'
-num_cols = num_cols.drop(['id', 'intervalo'])
+num_cols = num_cols.drop(['id', 'intervalo','chuva_hora','chuva_semana','chuva_mes','chuva_24h'])
 
 st.title("Estação Meteorológica IFPR-Campus Capanema")
 st.write("Aqui estão os dados da estação Local:")
 
-
-# criando dataframe de correlação
+# criando dataframe de correlação 1
 corr_df = df[num_cols].corr()
-
 # Plotando o gráfico de correlação
 st.write("Gráfico de correlação dos dados")
 fig, ax = plt.subplots(figsize=(10, 10))
@@ -64,7 +79,20 @@ sns.heatmap(corr_df, annot=True, cmap="coolwarm", ax=ax)
 st.pyplot(fig)
 def correlation_plot():
     fig, ax = plt.subplots(figsize=(10,10))
-    sns.heatmap(df.corr(), annot=True, cmap='coolwarm', linewidths=.5, ax=ax)
+    sns.heatmap(corr_df(), annot=True, cmap='coolwarm', ax=ax)
+    st.pyplot(fig)
+
+# criando dataframe de correlação 2
+corr = df_clean[num_cols].corr()
+# Plotando o gráfico de correlação
+st.write("Gráfico de correlação dos dados sem outliers")
+fig, ax = plt.subplots(figsize=(10, 10))
+sns.heatmap(corr, annot=True, cmap="coolwarm", ax=ax)
+st.pyplot(fig)
+
+def correlation_plot():
+    fig, ax = plt.subplots(figsize=(10,10))
+    sns.heatmap(corr(), annot=True, cmap='coolwarm', ax=ax)
     st.pyplot(fig)
 
 # Filtro na sidebar para selecionar as linhas de temperatura
@@ -76,12 +104,4 @@ if selected_columns:
     title = 'Correlação entre as variáveis de temperatura selecionadas'
     correlation_plot(new_df, title)
 
-# Plota o heatmap com todas as variáveis
-plt.figure(figsize=(10, 8))
-sns.heatmap(corr, annot=True, cmap='coolwarm')
-plt.title('Correlação entre as variáveis selecionadas')
-plt.xticks(rotation=45, ha='right')
-plt.yticks(rotation=0)
-plt.tight_layout()
-st.pyplot()
 
