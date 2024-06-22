9665a322dbde4366dd2d0cd63325474233ebbc94
Rogério <rogerio.dosantos@ifpr.edu.br>
2024-06-22 17:48:08 -0300

    UpDate

--- Multi-Input/Previsoes.py
+++ Multi-Input/Previsoes.py
@@ -1,75 +1,209 @@
 import pandas as pd
-import numpy as np
+import streamlit as st
+import time
+import random
+import requests
 
-# Carrega as previsões de cada modelo (substitua pelos nomes dos seus arquivos)
-previsoes_satelite = pd.read_csv('previsoes_satelite.csv')
-previsoes_sensores = pd.read_csv('previsoes_sensores.csv')
-previsoes_cultura = pd.read_csv('previsoes_cultura.csv')
-previsoes_estacao = pd.read_csv('previsoes_estacao.csv')
-previsoes_temporais = pd.read_csv('previsoes_temporais.csv')
 
-# Reseta o índice de cada DataFrame para garantir índices únicos
-previsoes_satelite = previsoes_satelite.reset_index(drop=True)
-previsoes_sensores = previsoes_sensores.reset_index(drop=True)
-previsoes_cultura = previsoes_cultura.reset_index(drop=True)
-previsoes_estacao = previsoes_estacao.reset_index(drop=True)
-previsoes_temporais = previsoes_temporais.reset_index(drop=True)
+def carregar_previsoes(nome_arquivo):
+    """Carrega as previsões de um modelo a partir de um arquivo CSV."""
+    try:
+        df = pd.read_csv (nome_arquivo)
+        return df
+    except FileNotFoundError:
+        st.error (f"Arquivo '{nome_arquivo}' não encontrado.")
+        return None
 
-# Combina as previsões em um único DataFrame
-previsoes_combinadas = pd.concat([
-    previsoes_satelite,
-    previsoes_sensores,
-    previsoes_cultura,
-    previsoes_estacao,
-    previsoes_temporais
-], axis=1)
 
-# Define os pesos para cada modelo em cada fase de desenvolvimento
-pesos = {
-    'Vegetativo': {
-        'satelite_solo': 0.1,
-        'satelite_agua': 0.1,
-        'satelite_vegetacao': 0.1,
-        'umidade_solo': 0.3,
-        'condutividade_eletrica': 0.3,
-        'temperatura_solo': 0.3,
-        'cultura_demanda': 0.5,
-        'estacao_precipitacao': 0.1,
-        'temporais_chuva': 0.0
-    },
-    'Floração': {
-        'satelite_solo': 0.1,
-        'satelite_agua': 0.1,
-        'satelite_vegetacao': 0.1,
-        'umidade_solo': 0.2,
-        'condutividade_eletrica': 0.2,
-        'temperatura_solo': 0.2,
-        'cultura_demanda': 0.6,
-        'estacao_precipitacao': 0.1,
-        'temporais_chuva': 0.0
-    },
-    'Colheita': {
-        'satelite_solo': 0.1,
-        'satelite_agua': 0.1,
-        'satelite_vegetacao': 0.1,
-        'umidade_solo': 0.1,
-        'condutividade_eletrica': 0.1,
-        'temperatura_solo': 0.1,
-        'cultura_demanda': 0.7,
-        'estacao_precipitacao': 0.1,
-        'temporais_chuva': 0.0
+def combinar_previsoes(previsoes_satelite, previsoes_sensores, previsoes_cultura, previsoes_estacao,
+                       previsoes_temporais):
+    """Combina as previsões dos modelos em um único DataFrame."""
+    previsoes_combinadas = pd.concat ([
+        previsoes_satelite,
+        previsoes_sensores,
+        previsoes_cultura,
+        previsoes_estacao,
+        previsoes_temporais
+    ], axis=1)
+    return previsoes_combinadas
+
+
+def calcular_previsao_final(previsoes_combinadas, estagio, pesos):
+    """Calcula a previsão final de irrigação com base no estágio da planta."""
+    # Cria uma coluna para o estágio de desenvolvimento
+    previsoes_combinadas['estagio_cultura'] = previsoes_combinadas['estagio_cultura'].astype ('category')
+
+    # Calcula a previsão final utilizando a média ponderada
+    previsoes_combinadas['previsao_final'] = 0
+    for modelo, peso in pesos[estagio].items ():
+        previsoes_combinadas['previsao_final'] += previsoes_combinadas[modelo] * peso
+
+    return previsoes_combinadas
+
+
+def obter_quantidade_agua(estagio, tipo_irrigacao, tipo_solo):
+    """Retorna a quantidade de água a ser aplicada com base na tabela de irrigação."""
+    tabela_irrigacao = {
+        'Vegetativo': {
+            'Aspersão': {
+                'Franco-arenoso': 50,
+                'Argiloso': 40,
+                'Areia': 60
+            },
+            'Gotejamento': {
+                'Franco-arenoso': 30,
+                'Argiloso': 25,
+                'Areia': 40
+            }
+        },
+        'Floração': {
+            'Aspersão': {
+                'Franco-arenoso': 40,
+                'Argiloso': 35,
+                'Areia': 50
+            },
+            'Gotejamento': {
+                'Franco-arenoso': 25,
+                'Argiloso': 20,
+                'Areia': 30
+            }
+        },
+        'Colheita': {
+            'Aspersão': {
+                'Franco-arenoso': 30,
+                'Argiloso': 25,
+                'Areia': 40
+            },
+            'Gotejamento': {
+                'Franco-arenoso': 15,
+                'Argiloso': 10,
+                'Areia': 20
+            }
+        }
     }
-}
+    return tabela_irrigacao[estagio][tipo_irrigacao][tipo_solo]
 
-# Cria uma coluna para o estágio de desenvolvimento
-previsoes_combinadas['estagio_cultura'] = previsoes_combinadas['estagio_cultura'].astype('category')
 
-# Calcula a previsão final utilizando a média ponderada
-previsoes_combinadas['previsao_final'] = 0
-for estagio in pesos:
-    for modelo, peso in pesos[estagio].items():
-        previsoes_combinadas.loc[previsoes_combinadas['estagio_cultura'] == estagio, 'previsao_final'] += previsoes_combinadas[modelo] * peso
+def get_weather_data(city):
+    """Obtém dados climáticos da API OpenWeatherMap."""
+    api_key = "342e60213afbec5b9b52e7f87d90248e"  # Substitua pela sua chave API do OpenWeatherMap
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    complete_url = base_url + "appid=" + api_key + "&q=" + city
+    response = requests.get (complete_url)
+    if response.status_code == 200:
+        data = response.json ()
+        main = data['main']
+        temp = round ((main['temp'] - 273.15), 1)  # Convertendo de Kelvin para Celsius
+        humidity = main['humidity']
+        return temp, humidity
+    else:
+        return None, None
 
-# Salva as previsões finais
-previsoes_combinadas.to_csv('previsoes_finais.csv', index=False)
-print("Previsões finais salvas em previsões_finais.csv")
+
+def main():
+    st.title ("Sys-Previsão de Irrigação Lúpulo")
+
+    # --- Barra Lateral ---
+    with st.sidebar:
+        st.subheader ("Escolha as Opções:")
+        estagio = st.selectbox ("Selecione o estágio da planta:", ['Vegetativo', 'Floração', 'Colheita'])
+        tipo_irrigacao = st.selectbox ("Selecione o tipo de irrigação:", ['Aspersão', 'Gotejamento'])
+        tipo_solo = st.selectbox ("Selecione o tipo de solo:", ['Franco-arenoso', 'Argiloso', 'Areia'])
+
+    # --- Conteúdo Principal ---
+    # Dados climáticos em tempo real
+    city = "Capanema, BR"  # Substitua pela cidade desejada
+    temp, humidity = get_weather_data (city)
+    st.markdown ("----------")
+    # Exibe os dados climáticos em um layout de matriz
+    st.markdown (
+        f"""
+        | **Dados Climáticos** | **Valor** |
+        |---|---|
+        | Temperatura do Ar | {temp}°C |
+        | Umidade do Ar | {humidity}% |
+        """
+    )
+    st.markdown ("----------")
+    # Simula um sensor de temperatura em tempo real (com valores aleatórios)
+    st.markdown (
+        f"""
+        | **Dados do Solo** | **Valor** |
+        |---|---|
+        | Temperatura do Solo (Tempo Real) | {round (random.uniform (15, 30), 1)}°C |
+        """
+    )
+
+    # Título do Sistema
+    st.markdown ("----------")
+
+    # Carregue as previsões de cada modelo
+    previsoes_satelite = carregar_previsoes ('previsoes_satelite.csv')
+    previsoes_sensores = carregar_previsoes ('previsoes_sensores.csv')
+    previsoes_cultura = carregar_previsoes ('previsoes_cultura.csv')
+    previsoes_estacao = carregar_previsoes ('previsoes_estacao.csv')
+    previsoes_temporais = carregar_previsoes ('previsoes_temporais.csv')
+
+    if all ([not df.empty for df in
+             [previsoes_satelite, previsoes_sensores, previsoes_cultura, previsoes_estacao, previsoes_temporais]]):
+        # Combine as previsões
+        previsoes_combinadas = combinar_previsoes (previsoes_satelite, previsoes_sensores, previsoes_cultura,
+                                                   previsoes_estacao, previsoes_temporais)
+
+        # Define os pesos para cada modelo em cada fase de desenvolvimento
+        pesos = {
+            'Vegetativo': {
+                'satelite_solo': 0.1,
+                'satelite_agua': 0.1,
+                'satelite_vegetacao': 0.1,
+                'umidade_solo': 0.3,
+                'condutividade_eletrica': 0.3,
+                'temperatura_solo': 0.3,
+                'cultura_demanda': 0.5,
+                'estacao_precipitacao': 0.1,
+                'temporais_chuva': 0.0
+            },
+            'Floração': {
+                'satelite_solo': 0.1,
+                'satelite_agua': 0.1,
+                'satelite_vegetacao': 0.1,
+                'umidade_solo': 0.2,
+                'condutividade_eletrica': 0.2,
+                'temperatura_solo': 0.2,
+                'cultura_demanda': 0.6,
+                'estacao_precipitacao': 0.1,
+                'temporais_chuva': 0.0
+            },
+            'Colheita': {
+                'satelite_solo': 0.1,
+                'satelite_agua': 0.1,
+                'satelite_vegetacao': 0.1,
+                'umidade_solo': 0.1,
+                'condutividade_eletrica': 0.1,
+                'temperatura_solo': 0.1,
+                'cultura_demanda': 0.7,
+                'estacao_precipitacao': 0.1,
+                'temporais_chuva': 0.0
+            }
+        }
+
+        # Calcula a previsão final
+        previsoes_combinadas = calcular_previsao_final (previsoes_combinadas, estagio, pesos)
+
+        # Obtém a quantidade de água da tabela de irrigação
+        quantidade_agua = obter_quantidade_agua (estagio, tipo_irrigacao, tipo_solo)
+
+        # Exibe o resultado
+        previsao_final = previsoes_combinadas[
+            'previsao_final'].mean ()  # Calcula a média das previsões para o estagio selecionado
+        if previsao_final >= 0.5:
+            st.success (
+                f"Irrigar! A previsão de irrigação para o estágio {estagio} é alta: {previsao_final:.2f}. Aplique {quantidade_agua} litros de água por m².")
+        else:
+            st.info (f"Não irrigar! A previsão de irrigação para o estágio {estagio} é baixa: {previsao_final:.2f}.")
+    else:
+        st.error ("Erro ao carregar os arquivos de previsão.")
+
+
+if __name__ == "__main__":
+    main ()
--- Multi-Input/Quadro_testes.py
+++ Multi-Input/Quadro_testes.py
@@ -1,52 +1,209 @@
 import pandas as pd
-import numpy as np
+import streamlit as st
+import time
 import random
+import requests
 
-# Define o número de linhas para cada conjunto de dados
-num_linhas = 1000
 
-# Define os estágios de desenvolvimento do lúpulo
-estagios = ['Vegetativo', 'Floração', 'Colheita']
+def carregar_previsoes(nome_arquivo):
+    """Carrega as previsões de um modelo a partir de um arquivo CSV."""
+    try:
+        df = pd.read_csv (nome_arquivo)
+        return df
+    except FileNotFoundError:
+        st.error (f"Arquivo '{nome_arquivo}' não encontrado.")
+        return None
 
-# Função para gerar dados aleatórios dentro de um intervalo
-def gerar_dados_aleatorios(minimo, maximo, num_linhas):
-    return np.random.uniform(minimo, maximo, num_linhas)
 
-# Gera os dados de previsão para cada modelo (valores entre 0 e 1)
-previsoes_satelite = pd.DataFrame({
-    'satelite_solo': gerar_dados_aleatorios(0, 1, num_linhas),
-    'satelite_agua': gerar_dados_aleatorios(0, 1, num_linhas),
-    'satelite_vegetacao': gerar_dados_aleatorios(0, 1, num_linhas),
-    'estagio_cultura': [random.choice(estagios) for _ in range(num_linhas)]
-})
+def combinar_previsoes(previsoes_satelite, previsoes_sensores, previsoes_cultura, previsoes_estacao,
+                       previsoes_temporais):
+    """Combina as previsões dos modelos em um único DataFrame."""
+    previsoes_combinadas = pd.concat ([
+        previsoes_satelite,
+        previsoes_sensores,
+        previsoes_cultura,
+        previsoes_estacao,
+        previsoes_temporais
+    ], axis=1)
+    return previsoes_combinadas
 
-previsoes_sensores = pd.DataFrame({
-    'umidade_solo': gerar_dados_aleatorios(0, 1, num_linhas),
-    'condutividade_eletrica': gerar_dados_aleatorios(0, 1, num_linhas),
-    'temperatura_solo': gerar_dados_aleatorios(0, 1, num_linhas),
-    'estagio_cultura': [random.choice(estagios) for _ in range(num_linhas)]
-})
 
-previsoes_cultura = pd.DataFrame({
-    'cultura_demanda': gerar_dados_aleatorios(0, 1, num_linhas),
-    'estagio_cultura': [random.choice(estagios) for _ in range(num_linhas)]
-})
+def calcular_previsao_final(previsoes_combinadas, estagio, pesos):
+    """Calcula a previsão final de irrigação com base no estágio da planta."""
+    # Cria uma coluna para o estágio de desenvolvimento
+    previsoes_combinadas['estagio_cultura'] = previsoes_combinadas['estagio_cultura'].astype ('category')
 
-previsoes_estacao = pd.DataFrame({
-    'estacao_precipitacao': gerar_dados_aleatorios(0, 1, num_linhas),
-    'estagio_cultura': [random.choice(estagios) for _ in range(num_linhas)]
-})
+    # Calcula a previsão final utilizando a média ponderada
+    previsoes_combinadas['previsao_final'] = 0
+    for modelo, peso in pesos[estagio].items ():
+        previsoes_combinadas['previsao_final'] += previsoes_combinadas[modelo] * peso
 
-previsoes_temporais = pd.DataFrame({
-    'temporais_chuva': gerar_dados_aleatorios(0, 1, num_linhas),
-    'estagio_cultura': [random.choice(estagios) for _ in range(num_linhas)]
-})
+    return previsoes_combinadas
 
-# Salva os dados de previsão em arquivos CSV separados
-previsoes_satelite.to_csv('previsoes_satelite.csv', index=False)
-previsoes_sensores.to_csv('previsoes_sensores.csv', index=False)
-previsoes_cultura.to_csv('previsoes_cultura.csv', index=False)
-previsoes_estacao.to_csv('previsoes_estacao.csv', index=False)
-previsoes_temporais.to_csv('previsoes_temporais.csv', index=False)
 
-print("Previsões ficticias salvas em arquivos CSV separados.")
+def obter_quantidade_agua(estagio, tipo_irrigacao, tipo_solo):
+    """Retorna a quantidade de água a ser aplicada com base na tabela de irrigação."""
+    tabela_irrigacao = {
+        'Vegetativo': {
+            'Aspersão': {
+                'Franco-arenoso': 50,
+                'Argiloso': 40,
+                'Areia': 60
+            },
+            'Gotejamento': {
+                'Franco-arenoso': 30,
+                'Argiloso': 25,
+                'Areia': 40
+            }
+        },
+        'Floração': {
+            'Aspersão': {
+                'Franco-arenoso': 40,
+                'Argiloso': 35,
+                'Areia': 50
+            },
+            'Gotejamento': {
+                'Franco-arenoso': 25,
+                'Argiloso': 20,
+                'Areia': 30
+            }
+        },
+        'Colheita': {
+            'Aspersão': {
+                'Franco-arenoso': 30,
+                'Argiloso': 25,
+                'Areia': 40
+            },
+            'Gotejamento': {
+                'Franco-arenoso': 15,
+                'Argiloso': 10,
+                'Areia': 20
+            }
+        }
+    }
+    return tabela_irrigacao[estagio][tipo_irrigacao][tipo_solo]
+
+
+def get_weather_data(city):
+    """Obtém dados climáticos da API OpenWeatherMap."""
+    api_key = "342e60213afbec5b9b52e7f87d90248e"  # Substitua pela sua chave API do OpenWeatherMap
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    complete_url = base_url + "appid=" + api_key + "&q=" + city
+    response = requests.get (complete_url)
+    if response.status_code == 200:
+        data = response.json ()
+        main = data['main']
+        temp = round ((main['temp'] - 273.15), 1)  # Convertendo de Kelvin para Celsius
+        humidity = main['humidity']
+        return temp, humidity
+    else:
+        return None, None
+
+
+def main():
+    st.title ("Sys-Previsão de Irrigação Lúpulo")
+
+    # --- Barra Lateral ---
+    with st.sidebar:
+        st.subheader ("Escolha as Opções:")
+        estagio = st.selectbox ("Selecione o estágio da planta:", ['Vegetativo', 'Floração', 'Colheita'])
+        tipo_irrigacao = st.selectbox ("Selecione o tipo de irrigação:", ['Aspersão', 'Gotejamento'])
+        tipo_solo = st.selectbox ("Selecione o tipo de solo:", ['Franco-arenoso', 'Argiloso', 'Areia'])
+
+    # --- Conteúdo Principal ---
+    # Dados climáticos em tempo real
+    city = "Capanema, BR"  # Substitua pela cidade desejada
+    temp, humidity = get_weather_data (city)
+    st.markdown ("----------")
+    # Exibe os dados climáticos em um layout de matriz
+    st.markdown (
+        f"""
+        | **Dados Climáticos** | **Valor** |
+        |---|---|
+        | Temperatura do Ar | {temp}°C |
+        | Umidade do Ar | {humidity}% |
+        """
+    )
+    st.markdown ("----------")
+    # Simula um sensor de temperatura em tempo real (com valores aleatórios)
+    st.markdown (
+        f"""
+        | **Dados do Solo** | **Valor** |
+        |---|---|
+        | Temperatura do Solo (Tempo Real) | {round (random.uniform (15, 30), 1)}°C |
+        """
+    )
+
+    # Título do Sistema
+    st.markdown ("----------")
+
+    # Carregue as previsões de cada modelo
+    previsoes_satelite = carregar_previsoes ('previsoes_satelite.csv')
+    previsoes_sensores = carregar_previsoes ('previsoes_sensores.csv')
+    previsoes_cultura = carregar_previsoes ('previsoes_cultura.csv')
+    previsoes_estacao = carregar_previsoes ('previsoes_estacao.csv')
+    previsoes_temporais = carregar_previsoes ('previsoes_temporais.csv')
+
+    if all ([not df.empty for df in
+             [previsoes_satelite, previsoes_sensores, previsoes_cultura, previsoes_estacao, previsoes_temporais]]):
+        # Combine as previsões
+        previsoes_combinadas = combinar_previsoes (previsoes_satelite, previsoes_sensores, previsoes_cultura,
+                                                   previsoes_estacao, previsoes_temporais)
+
+        # Define os pesos para cada modelo em cada fase de desenvolvimento
+        pesos = {
+            'Vegetativo': {
+                'satelite_solo': 0.1,
+                'satelite_agua': 0.1,
+                'satelite_vegetacao': 0.1,
+                'umidade_solo': 0.3,
+                'condutividade_eletrica': 0.3,
+                'temperatura_solo': 0.3,
+                'cultura_demanda': 0.5,
+                'estacao_precipitacao': 0.1,
+                'temporais_chuva': 0.0
+            },
+            'Floração': {
+                'satelite_solo': 0.1,
+                'satelite_agua': 0.1,
+                'satelite_vegetacao': 0.1,
+                'umidade_solo': 0.2,
+                'condutividade_eletrica': 0.2,
+                'temperatura_solo': 0.2,
+                'cultura_demanda': 0.6,
+                'estacao_precipitacao': 0.1,
+                'temporais_chuva': 0.0
+            },
+            'Colheita': {
+                'satelite_solo': 0.1,
+                'satelite_agua': 0.1,
+                'satelite_vegetacao': 0.1,
+                'umidade_solo': 0.1,
+                'condutividade_eletrica': 0.1,
+                'temperatura_solo': 0.1,
+                'cultura_demanda': 0.7,
+                'estacao_precipitacao': 0.1,
+                'temporais_chuva': 0.0
+            }
+        }
+
+        # Calcula a previsão final
+        previsoes_combinadas = calcular_previsao_final (previsoes_combinadas, estagio, pesos)
+
+        # Obtém a quantidade de água da tabela de irrigação
+        quantidade_agua = obter_quantidade_agua (estagio, tipo_irrigacao, tipo_solo)
+
+        # Exibe o resultado
+        previsao_final = previsoes_combinadas[
+            'previsao_final'].mean ()  # Calcula a média das previsões para o estagio selecionado
+        if previsao_final >= 0.5:
+            st.success (
+                f"Irrigar! A previsão de irrigação para o estágio {estagio} é alta: {previsao_final:.2f}. Aplique {quantidade_agua} litros de água por m².")
+        else:
+            st.info (f"Não irrigar! A previsão de irrigação para o estágio {estagio} é baixa: {previsao_final:.2f}.")
+    else:
+        st.error ("Erro ao carregar os arquivos de previsão.")
+
+
+if __name__ == "__main__":
+    main ()
